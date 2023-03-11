0f272e432057ec53e38ff549dba81fd3de2007e4
samoradi94 <samoradi94@gmail.com>
2023-03-11 17:32:56 +0330

    basic image processing solution implemented

--- main.py
+++ main.py
@@ -1,71 +1,55 @@
 import pyautogui
 import webbrowser
 import time
+import numpy as np
 
 
-def get_mouse_position():
-    for i in range(0, 50):
+# def get_mouse_position():
 
-        x1, y = pyautogui.position()
-        x2 = x1 + 10
+#     for i in range(0, 50):
+#         x1, y = pyautogui.position()
+#         x2 = x1 + 10
 
-        for x in range(x1, x2):
-            r, g, b = pyautogui.pixel(x, y)
-            if r == 83 and g == 83 and b == 83:
-                print('obj')
-
-        # position: x = 1028 1045 y = 709
-        # color : BG= 247 247 247     object = 83 83 83
+#         for x in range(x1, x2):
+#             r, g, b = pyautogui.pixel(x, y)
+            
 
 
 def main():
     url = "https://chromedino.com/"
-
+    
     webbrowser.open(url, new=0, autoraise=True)
-
     time.sleep(3)
-
+    
     # space to start the game
     pyautogui.keyDown('space')
     pyautogui.keyUp('space')
 
 
-    for i in range(1, 50):
-        # x, y = pyautogui.position()
-        # dino_x, dino_y = pyautogui.position()
-        # a_x = dino_x + 200
-        # a_y = dino_y
-        # px = pyautogui.pixel(a_x, a_y)
-        # if px.red == 83 and px.green == 83 and px.blue == 83:
-        #     print('yes')
-        #     pyautogui.keyDown('space')
-        #     pyautogui.keyUp('space')
+    for i in range(1, 600):
+        
+        thresh = 200
+        fn = lambda x : 255 if x > thresh else 0
+  
+        im1 = pyautogui.screenshot(region=(351,200, 695, 150)).convert('L').point(fn, mode='1')
+        # dino position: 109 124   rgb = 247, 247, 247
 
+        # obj = im1.crop(box=(181, 92, 282, 140))
+        # obj = im1.crop(box=(163, 93, 277, 148)) # 6100    
+        # obj = im1.crop(box=(137, 93, 277,  148)) # 7520 best score 491
+        # obj = im1.crop(box=(163, 93, 287,  148)) #  6650
+        # obj = im1.crop(box=(163, 93, 270,  148)) #  5700 best score 504
+        obj = im1.crop(box=(153, 93, 270,  148)) #  6270  best score 323
+        # obj = im1.crop(box=(154, 93, 277, 148)) # 6600
+        # obj = im1.crop(box=(157, 87, 265,147)) # 6300
+        
+        
+        # obj.save(f'obj_{np.asarray(obj, dtype="int32").sum()}.png')
 
-        # x, y = 821, 722
-        # print(x,y)
-        # print(pyautogui.pixel(x,y))
-        #
-        # if pyautogui.pixel(x + 20, y).blue == 83:
-        #     pyautogui.keyDown('space')
-        #     pyautogui.keyUp('space')
-
-        dino_x = 794  #842
-        dino_y = 722
-        # if pyautogui.pixel(dino_x + 50, dino_y).blue == 83:
-        #     print('d')
-        #     pyautogui.keyDown('space')
-        #     pyautogui.keyUp('space')
-        # for x in range(dino_x + 300 , dino_x + 400):
-        #     px = pyautogui.pixel(x  , dino_y)
-        #     if px.red == 83 and px.green == 83 and px.blue == 83:
-        #         print('yes')
-        #         pyautogui.keyDown('space')
-        #         pyautogui.keyUp('space')
-        # if px.red != 247 and px.green != 247 and px.blue != 247:
-        #     pyautogui.keyDown('space')
-        #     pyautogui.keyUp('space')
-
-
-get_mouse_position()
+        if np.asarray(obj, dtype="int32").sum() < 6270    :
+            pyautogui.keyDown('space')
+            pyautogui.keyUp('space')
+        
+main()
+# get_mouse_position()
 
